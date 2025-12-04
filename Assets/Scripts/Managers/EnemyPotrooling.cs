d485474b9d0a2a63ecb5ce0d990d9340787036e2
Qvatelich <159568222+Qvatelich@users.noreply.github.com>
2025-12-04 22:52:33 +0300

    0.3.4
    Я устал

---
+++ Assets/Scripts/Managers/EnemyPotrooling.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public sealed class EnemyPotrooling : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3f;
+
+    private bool _potrool;
+    private Transform _player;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            _potrool = true;
+        Debug.Log("Pfs");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            _potrool = false;
+    }
+    private void FixedUpdate()
+    {
+        if (_potrool)
+        {
+            Vector3 targetPos = _player.transform.position;
+            targetPos.y = transform.position.y;
+            transform.position = Vector3.Lerp(transform.position,targetPos, _speed);
+        }
+    }
+}
